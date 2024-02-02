c2b12b772c653b8f4fb53cf86f8b1c00a1774cbd
Nathaniel Getachew <nathigechmaranata@gmail.com>
2024-02-02 12:01:09 +0300

    finishing the model

---
+++ model/Categories/Category.js
@@ -0,0 +1,41 @@
+const mongoose = require
+("mongoose")
+const categorySchema =  new mongoose.Schema({
+
+name: {
+    type: String,
+    required: true,
+},
+author: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+},
+shares: {
+    type: Number,
+    default:0,
+},
+
+likes: [
+    {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+    }
+],
+
+posts: 
+    {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Post"
+    },
+
+},
+{
+timestamps: true,
+}
+ 
+);
+// compile schema to model
+
+const Category = mongoose.model("Category",categorySchema);
+module.exports = Category;
