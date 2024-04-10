d6d91eef7d6ab1d0795657ff0b8ec68f77c2204e
Nathaniel Getachew <nathigechmaranata@gmail.com>
2024-04-10 12:01:26 +0300

    Homepage and Login Components done

--- blogify-front-end/src/App.js
+++ blogify-front-end/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter,Routes,Route }     from "react-router-dom"
+import Homepage from "./Component/HomePage/Homepage"
+import Login from "./Component/HomePage/Users/Login"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+export default function App() {
+   return (
+<BrowserRouter>
+  <Routes>
+    <Route path ="" element={<Homepage/>}></Route>
+    <Route path ="/login" element={<Login/>}></Route>
+
+
+    
+  </Routes>
+</BrowserRouter>
+  )
 }
 
-export default App;
